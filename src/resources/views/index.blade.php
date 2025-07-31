ded21b7f54ce3c8925ed521177526fef386993d7
Martijn Tetaert <martijn@libaro.be>
2025-07-31 09:45:14 +0200

    Update styling

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -85,7 +85,7 @@
     @endif
 </div>
 <div class="sticky bottom-0 bg-gray-800 text-white p-4 mt-4">
-    <div class="px-4 flex justify-end">
+    <div class="flex justify-end">
         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded hover:cursor-pointer"
                 onclick="submitForm()">
             Submit
